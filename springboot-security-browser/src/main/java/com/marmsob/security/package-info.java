c1924ba548681969150a1285e5ccaf8a53de10e3
jevno <rucky2013@163.com>
2018-06-27 15:50:33 +0800

    init

---
+++ springboot-security-browser/src/main/java/com/marmsob/security/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @title 		
+ * @description	
+ * @usage		
+ * @copyright	Copyright 2017  marsmob Corporation. All rights reserved.
+ * @company		marsmob
+ * @author		jevno
+ * @create		2018年6月27日下午3:28:37
+ */
+package com.marmsob.security;
