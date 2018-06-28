ebbf40433e30e6c91b8739166b0c280596c56493
jevno <rucky2013@163.com>
2018-06-28 11:33:44 +0800

    取消security验证

--- springboot-security-demo/src/main/java/com/marsmob/security/Application.java
+++ springboot-security-demo/src/main/java/com/marsmob/security/Application.java
@@ -1,14 +1,27 @@
 package com.marsmob.security;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+/**
+ * security.basic.enabled = false 已经过时
+ * 改为@EnableAutoConfiguration(exclude= {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+ */
+@EnableAutoConfiguration(exclude= {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class Application {
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@GetMapping("hello")
+	public String hello()
+	{
+		return "hello springboot-security";
+	}
 }
